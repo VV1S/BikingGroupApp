e9ec313324fc69cbb4531648c25b671e1ffaa0d1
Robert <robertjwisniowski@gmail.com>
2024-10-06 17:41:47 +0200

    Initial commit.

---
+++ src/main/java/com/bikingGroup/BikingGroupApp/BikingGroupAppApplication.java
@@ -0,0 +1,13 @@
+package com.bikingGroup.BikingGroupApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikingGroupAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikingGroupAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bikingGroup/BikingGroupApp/BikingGroupAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikingGroup.BikingGroupApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikingGroupAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
